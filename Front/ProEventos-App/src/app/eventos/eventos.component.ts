903a25467e3b32131dfe18eb2a19e1371f917ab0
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2023-02-23 00:16:57 -0400

    consumindo da api back

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,27 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
+  //Criando Lista de objetos manual para testes
+  //  public eventos: any = [
+  //   {
+  //    Tema: 'Angular',
+  //    Local: 'Campo Grande'
+  //   },
+  //   {
+  //     Tema: 'Angular11',
+  //     Local: 'Rio de Janeiro'
+  //    },
+  //    {
+  //     Tema: 'Angular14',
+  //     Local: 'Parana'
+  //    }
+  // ]
 
-   public eventos: any = [
-    {
-     Tema: 'Angular',
-     Local: 'Campo Grande'
-    },
-    {
-      Tema: 'Angular11',
-      Local: 'Rio de Janeiro'
-     },
-     {
-      Tema: 'Angular14',
-      Local: 'Parana'
-     }
-  ]
+  public eventos: any;
 
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.getEventos();
+  }
 
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      (response) => (this.eventos = response),
+      (error) => console.log(error)
+    );
+  }
 }
