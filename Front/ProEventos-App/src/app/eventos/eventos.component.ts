74870bd446a81cf6b7e2e4bd9221c95869c1374c
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2023-02-26 15:41:54 -0400

    filtro grid

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -24,11 +24,28 @@ export class EventosComponent {
   // ]
 
   public eventos: any = [];
+  public eventosFiltrados: any = []
   larguraImagem: number = 100;
   margemImagem: number = 2;
   exibirImagem: boolean = true;
-  filtroLista: string = '';
+  private _filtroLista: string = '';
 
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
@@ -41,8 +58,11 @@ export class EventosComponent {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => (this.eventos = response),
-      (error) => console.log(error)
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos
+      },
+      error => console.log(error)
     );
   }
 }
